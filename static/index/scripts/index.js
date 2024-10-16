6e6cf5ff16ce97e6ca59a94bac0a7cb9c709666f
uvaprol <koryakin_ad@mail.ru>
2024-10-16 21:15:12 +0300

    check box
    animation with Goju Satoru`s the reverse technique

--- static/index/scripts/index.js
+++ static/index/scripts/index.js
@@ -1,10 +1,16 @@
 function checked(id){
     const inputs = document.getElementsByName('myRadio')
     const input = document.getElementById(id)
-    for (i of inputs){
-        console.log(i, input)
-        if (i != input){
-            i.checked = false
+    const spans = document.getElementsByTagName('span')
+    const span = document.getElementById('s_'+id)
+    for (let i = 0; i < inputs.length; i++){
+        if (inputs[i] != input){
+            inputs[i].checked = false
+            spans[i].style.width = '0px'
+            spans[i].style.height = '0px'
         }
     }
+    input.checked = true
+    span.style.width = '12px'
+    span.style.height = '12px'
 }
