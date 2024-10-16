fd90b716c0194af1614ada15e6f8f5d47ddf28a6
uvaprol <koryakin_ad@mail.ru>
2024-10-16 21:41:50 +0300

    checkbox
    update animation checkbox logick

--- static/index/scripts/index.js
+++ static/index/scripts/index.js
@@ -1,16 +1,18 @@
-function checked(id){
-    const inputs = document.getElementsByName('myRadio')
+function box_checked(id){
     const input = document.getElementById(id)
+    const inputs = document.getElementsByName('myRadio')
     const spans = document.getElementsByTagName('span')
     const span = document.getElementById('s_'+id)
-    for (let i = 0; i < inputs.length; i++){
-        if (inputs[i] != input){
+    event.preventDefault()
+    if (input.checked == false){       
+        for (let i = 0; i < inputs.length; i++){
             inputs[i].checked = false
             spans[i].style.width = '0px'
             spans[i].style.height = '0px'
         }
+        input.checked = true
+        span.style.width = '12px'
+        span.style.height = '12px'
     }
-    input.checked = true
-    span.style.width = '12px'
-    span.style.height = '12px'
+    
 }
