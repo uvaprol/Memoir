fcefaaf8d08607b69608bc79e7c8a3b5185db8f0
uvaprol <koryakin_ad@mail.ru>
2024-10-25 21:02:18 +0300

    update user check

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request
-import pandas as pd
 import json
 
-
-DEV_MODE = False
-app = Flask(__name__)
 def load_DB():
     with open('DATABASE/DB.json', 'r') as json_file:
         data = json.load(json_file)
@@ -13,26 +9,26 @@ def get_users():
     with open('DATABASE/LOGINS.json', 'r') as json_file:
         users = json.load(json_file)
     return users
-# print(load_DB()['uvaprol']['Password'])
+
+
+DEV_MODE = False
+app = Flask(__name__)
+
 @app.route('/')
 def main_render():
     return render_template('index.html')
 
 @app.route('/enterance')
-def Enter_or_Registration():
+def enter_render():
     return render_template('enterance.html')
 
 @app.route('/enter')
 def user_check():
-
     login = request.args['login']
     password = request.args['password']
     users = get_users()
-    try:
-        if users[login] == password:
-            return 'true'
-        return 'false'
-    except:
-        return 'false'
+    if users.get(login) == password:
+        return 'true'
+    return 'false'
 
 app.run(host='0.0.0.0', port=80, debug=DEV_MODE)
