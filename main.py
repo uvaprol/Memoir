a690a8069213c5e344f633322fe78a965e9c8ca4
uvaprol <koryakin_ad@mail.ru>
2024-12-01 01:11:42 +0300

    make beautiful sql request

--- main.py
+++ main.py
@@ -5,7 +5,23 @@ import sqlite3
 def get_memoirs(name: str, year: str = date.today().year, month: str = date.today().month, memoirs: dict = {}) -> dict:
     connection = sqlite3.connect(DB_CONNECT[1])
     cursor = connection.cursor()
-    cursor.execute(f'SELECT {ROWS[1][4]}, {ROWS[1][3]}, {ROWS[1][5]}, {ROWS[1][6]}, {ROWS[1][7]}, {ROWS[1][8]} FROM {TABLES[1]} WHERE {ROWS[1][0]} = "{name}" AND {ROWS[1][1]} = {year} AND {ROWS[1][2]} = {month} ORDER BY {ROWS[1][4]} ASC, {ROWS[1][5]} ASC;')
+    cursor.execute(
+        f'SELECT '
+        f'{ROWS[1][4]}, '
+        f'{ROWS[1][3]}, '
+        f'{ROWS[1][5]}, '
+        f'{ROWS[1][6]}, '
+        f'{ROWS[1][7]}, '
+        f'{ROWS[1][8]} '
+        f'FROM {TABLES[1]} '
+        f'WHERE '
+        f'{ROWS[1][0]} = "{name}" '
+        f'AND {ROWS[1][1]} = {year} '
+        f'AND {ROWS[1][2]} = {month} '
+        f'ORDER BY '
+        f'{ROWS[1][4]} ASC, '
+        f'{ROWS[1][5]} ASC;'
+    )
     data = cursor.fetchall()
     connection.close()
     for d in data:
@@ -28,7 +44,24 @@ def get_calendar(user: str, day: int = datetime.today().weekday(), todate: str =
                 day = 1
                 week += 1
             if m == tm and i >= td or m > tm:
-                cursor.execute(f'INSERT INTO {TABLES[1]} ({ROWS[1][0]}, {ROWS[1][1]}, {ROWS[1][2]}, {ROWS[1][3]}, {ROWS[1][4]}, {ROWS[1][5]}, {ROWS[1][6]}) VALUES ("{user}", {ty}, {m}, {i}, {week}, {day}, "")')
+                cursor.execute(
+                    f'INSERT INTO {TABLES[1]} '
+                    f'({ROWS[1][0]}, '
+                    f'{ROWS[1][1]}, '
+                    f'{ROWS[1][2]}, '
+                    f'{ROWS[1][3]}, '
+                    f'{ROWS[1][4]}, '
+                    f'{ROWS[1][5]}, '
+                    f'{ROWS[1][6]}) '
+                    f'VALUES '
+                    f'("{user}", '
+                    f'{ty}, '
+                    f'{m}, '
+                    f'{i}, '
+                    f'{week}, '
+                    f'{day}, '
+                    f'"")'
+                )
             day += 1
     connection.commit()
     connection.close()
@@ -36,7 +69,14 @@ def get_calendar(user: str, day: int = datetime.today().weekday(), todate: str =
 def check_user_data(name: str, year: str = date.today().year) -> bool:
     connection = sqlite3.connect(DB_CONNECT[1])
     cursor = connection.cursor()
-    cursor.execute(f'SELECT * FROM {TABLES[1]} WHERE {ROWS[1][0]} = "{name}" AND {ROWS[1][1]} = "{year}" LIMIT 1')
+    cursor.execute(
+        f'SELECT * '
+        f'FROM {TABLES[1]} '
+        f'WHERE '
+        f'{ROWS[1][0]} = "{name}" '
+        f'AND {ROWS[1][1]} = "{year}" '
+        f'LIMIT 1'
+    )
     data = cursor.fetchone()
     connection.close()
     if data == None:
@@ -45,7 +85,13 @@ def check_user_data(name: str, year: str = date.today().year) -> bool:
 def get_reg_data(name: str) -> tuple:
     connection = sqlite3.connect(DB_CONNECT[0])
     cursor = connection.cursor()
-    cursor.execute(f'SELECT * FROM {TABLES[0]} WHERE {ROWS[0][0]} = "{name}" LIMIT 1')
+    cursor.execute(
+        f'SELECT * '
+        f'FROM {TABLES[0]} '
+        f'WHERE '
+        f'{ROWS[0][0]} = "{name}" '
+        f'LIMIT 1'
+    )
     user = cursor.fetchone()
     connection.close()
     return user
@@ -53,7 +99,16 @@ def new_reg(login: str, passwor: str, mail: str) -> bool:
     if get_reg_data(login) == None:
         connection = sqlite3.connect(DB_CONNECT[0])
         cursor = connection.cursor()
-        cursor.execute(f'INSERT INTO {TABLES[0]} ({ROWS[0][0]}, {ROWS[0][1]}, {ROWS[0][2]}) VALUES ("{login}", "{passwor}", "{mail}")')
+        cursor.execute(
+            f'INSERT INTO {TABLES[0]} '
+            f'({ROWS[0][0]}, '
+            f'{ROWS[0][1]}, '
+            f'{ROWS[0][2]}) '
+            f'VALUES '
+            f'("{login}", '
+            f'"{passwor}", '
+            f'"{mail}")'
+        )
         connection.commit()
         connection.close()
         return True
